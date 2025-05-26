feef38a30cc5727cb312442f8be8485d244c8b86
Kerem Uslular <kerem.uslular@pensasystems.com>
2025-05-26 11:05:53 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "PensaLibtorch",
-      url: "https://github.com/keremu-pensa/pensa-libtorch-vision/releases/download/v1.0.0/PensaLibtorch.xcframework.zip",
+      url: "https://github.com/keremu-pensa/pensa-libtorch-vision/releases/download/1.0.0/PensaLibtorch.xcframework.zip",
       checksum: "a5419af1392c76e4de09a9c8a05705a39a2652c2d95aa26eba0cb8cff82746ef"
     )
   ]
